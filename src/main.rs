f2d8f68ab618af3496975d399d99fc0c7e8e0a8b
Rick Richardson <rick.richardson@gmail.com>
2021-09-21 08:26:28 -0700

    adding much more forgiving error handling and additional json Value parsers

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use futures_util::{ StreamExt, io::{ AsyncBufReadExt, BufReader, AllowStdIo }};
-use std::io::{ Write, Error as IoError, ErrorKind, Cursor };
-use std::sync::Arc;
-use tokio::sync::mpsc::channel;
+use futures_util::{
+    io::{AllowStdIo, AsyncBufReadExt, BufReader},
+    StreamExt,
+};
 use serde_json::Value;
-use termcolor::{BufferWriter, Buffer, Color, ColorChoice, ColorSpec, WriteColor};
+use std::io::{Cursor, Error as IoError, ErrorKind, Write};
+use std::sync::Arc;
+use termcolor::{Buffer, BufferWriter, Color, ColorChoice, ColorSpec, WriteColor};
+use tokio::sync::mpsc::channel;
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 2)]
 pub async fn main() -> Result<(), std::io::Error> {
@@ -11,16 +14,16 @@ pub async fn main() -> Result<(), std::io::Error> {
     let mut lines = BufReader::new(stdin).lines();
     let (tx, mut rx) = channel::<Buffer>(100);
     let color = if atty::is(atty::Stream::Stdout) {
-            ColorChoice::Auto
-        } else {
-            ColorChoice::Never
-        };
+        ColorChoice::Auto
+    } else {
+        ColorChoice::Never
+    };
     let bufwtr = Arc::new(BufferWriter::stdout(color));
     let writer = bufwtr.clone();
 
     tokio::spawn(async move {
         while let Some(buf) = rx.recv().await {
-            writer.print(&buf).expect("writer print");
+            writer.print(&buf)?;
         }
         Ok::<_, std::io::Error>(())
     });
@@ -28,11 +31,14 @@ pub async fn main() -> Result<(), std::io::Error> {
         let line = line?;
         if let Ok(record) = serde_json::from_str(&line) {
             let mut buf = bufwtr.buffer();
-            process_record(record, &mut buf).expect("process_record");
+            if let Err(_) = process_record(record, &mut buf) {
+                buf.write(line.as_bytes())?;
+            }
+            buf.write(b"\n")?;
             let _ = tx.send(buf).await;
         } else {
             let mut buf = bufwtr.buffer();
-            buf.write(line.as_bytes()).expect("buf write");
+            buf.write(line.as_bytes())?;
             buf.write(b"\n")?;
             let _ = tx.send(buf).await;
         }
@@ -45,7 +51,11 @@ fn process_record(mut rec: Value, outbuf: &mut Buffer) -> Result<(), std::io::Er
         if let Some(ts) = record.get("ts") {
             if let Some(ts) = ts.as_str() {
                 outbuf.write(b"[")?;
-                outbuf.set_color(ColorSpec::new().set_intense(true).set_fg(Some(Color::White)))?;
+                outbuf.set_color(
+                    ColorSpec::new()
+                        .set_intense(true)
+                        .set_fg(Some(Color::White)),
+                )?;
                 outbuf.write(ts.as_bytes())?;
                 outbuf.set_color(&ColorSpec::new())?;
                 outbuf.write(b"]")?;
@@ -72,7 +82,11 @@ fn process_record(mut rec: Value, outbuf: &mut Buffer) -> Result<(), std::io::Er
                     }
                     30 => {
                         outbuf.write(b"[")?;
-                        outbuf.set_color(ColorSpec::new().set_intense(true).set_fg(Some(Color::Green)))?;
+                        outbuf.set_color(
+                            ColorSpec::new()
+                                .set_intense(true)
+                                .set_fg(Some(Color::Green)),
+                        )?;
                         outbuf.write(b" INFO")?;
                         outbuf.set_color(&ColorSpec::new())?;
                         outbuf.write(b"]")?;
@@ -88,7 +102,11 @@ fn process_record(mut rec: Value, outbuf: &mut Buffer) -> Result<(), std::io::Er
                     }
                     50 => {
                         outbuf.write(b"[")?;
-                        outbuf.set_color(ColorSpec::new().set_intense(true).set_fg(Some(Color::Yellow)))?;
+                        outbuf.set_color(
+                            ColorSpec::new()
+                                .set_intense(true)
+                                .set_fg(Some(Color::Yellow)),
+                        )?;
                         outbuf.write(b"ERROR")?;
                         outbuf.set_color(&ColorSpec::new())?;
                         outbuf.write(b"]")?;
@@ -96,7 +114,11 @@ fn process_record(mut rec: Value, outbuf: &mut Buffer) -> Result<(), std::io::Er
                     }
                     60 => {
                         outbuf.write(b"[")?;
-                        outbuf.set_color(ColorSpec::new().set_intense(true).set_fg(Some(Color::Magenta)))?;
+                        outbuf.set_color(
+                            ColorSpec::new()
+                                .set_intense(true)
+                                .set_fg(Some(Color::Magenta)),
+                        )?;
                         outbuf.write(b"ALERT")?;
                         outbuf.set_color(&ColorSpec::new())?;
                         outbuf.write(b"]")?;
@@ -104,7 +126,9 @@ fn process_record(mut rec: Value, outbuf: &mut Buffer) -> Result<(), std::io::Er
                     }
                     70 => {
                         outbuf.write(b"[")?;
-                        outbuf.set_color(ColorSpec::new().set_intense(true).set_fg(Some(Color::Red)))?;
+                        outbuf.set_color(
+                            ColorSpec::new().set_intense(true).set_fg(Some(Color::Red)),
+                        )?;
                         outbuf.write(b"FATAL")?;
                         outbuf.set_color(&ColorSpec::new())?;
                         outbuf.write(b"]")?;
@@ -148,17 +172,21 @@ fn process_record(mut rec: Value, outbuf: &mut Buffer) -> Result<(), std::io::Er
                 record.remove("message");
             }
         }
-        record.iter().for_each(|(k,v)| {
+        record.iter().for_each(|(k, v)| {
             let _ = outbuf.write(b" ");
             let _ = outbuf.write(k.as_bytes());
             let _ = outbuf.write(b"=");
             let _ = outbuf.write(rec_to_bytes(v).unwrap().as_slice());
         });
-        outbuf.write(b"\n")?;
         Ok(())
     } else {
-        println!("rec: {:?}", rec);
-        Err(IoError::new(ErrorKind::Other, "not a javascript object"))
+        match serde_json::to_vec(&rec) {
+            Err(err) => return Err(IoError::new(ErrorKind::Other, format!("{}", err))),
+            Ok(buf) => {
+                outbuf.write(buf.as_slice())?;
+            }
+        }
+        Ok(())
     }
 }
 
@@ -170,12 +198,41 @@ fn rec_to_bytes(value: &Value) -> Result<Vec<u8>, IoError> {
                 res.write(b"\n    ")?;
                 res.write(k.as_bytes())?;
                 res.write(b"=")?;
-                let o = serde_json::to_vec(v).map_err(|e| IoError::new(ErrorKind::Other, format!("{}", e)))?;
+                let o = if let Ok(buf) = serde_json::to_vec(v)
+                    .map_err(|e| IoError::new(ErrorKind::Other, format!("{}", e)))
+                {
+                    buf
+                } else {
+                    b"?obj?".to_vec()
+                };
                 res.write(o.as_slice())?;
             }
             res.write(b"\n")?;
             Ok(res.into_inner())
-        },
-        _ => serde_json::to_vec(value).map_err(|e| IoError::new(ErrorKind::Other, format!("{}", e)))
+        }
+        Value::Array(a) => {
+            let mut res = Cursor::new(vec![]);
+            res.write(b"[")?;
+            for v in a.iter() {
+                let o = if let Ok(buf) = serde_json::to_vec(v)
+                    .map_err(|e| IoError::new(ErrorKind::Other, format!("{}", e)))
+                {
+                    buf
+                } else {
+                    b"?arr?".to_vec()
+                };
+                res.write(o.as_slice())?;
+                res.write(b", ")?;
+            }
+            res.write(b"]")?;
+            Ok(res.into_inner())
+        }
+        _ => {
+            if let Ok(buf) = serde_json::to_vec(value) {
+                Ok(buf)
+            } else {
+                Ok(b"???".to_vec())
+            }
+        }
     }
 }
